cbd53c40a53591294eaafffaec490629d11afe92
13817206450 <defeat_zy@163.com>
2020-04-13 16:40:19 +0800

    首次

---
+++ src/test/java/com/bao/springboot/Springboot04WebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bao.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04WebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
